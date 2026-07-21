63622d5ddf662d6ae5dec1b3d41e4e6e50ca6afc
panhongmin001 <panhongmin001@ke.com>
2026-07-21 18:18:32 +0800

    ui 样式5

--- chongwu/apps/wxapp/custom-tab-bar/index.js
+++ chongwu/apps/wxapp/custom-tab-bar/index.js
@@ -4,19 +4,23 @@ Component({
     list: [
       {
         pagePath: "/pages/index/index",
-        icon: "🏠"
+        icon: "/assets/icons/tab-home.png",
+        iconActive: "/assets/icons/tab-home-active.png"
       },
       {
         pagePath: "/pages/service/service",
-        icon: "▦"
+        icon: "/assets/icons/tab-service.png",
+        iconActive: "/assets/icons/tab-service-active.png"
       },
       {
         pagePath: "/pages/idle/idle",
-        icon: "⇄"
+        icon: "/assets/icons/tab-idle.png",
+        iconActive: "/assets/icons/tab-idle-active.png"
       },
       {
         pagePath: "/pages/profile/profile",
-        icon: "👤"
+        icon: "/assets/icons/tab-profile.png",
+        iconActive: "/assets/icons/tab-profile-active.png"
       }
     ]
   },
